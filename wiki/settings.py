9648a1b8532014c8c29c1248a0a263148d9e84e4
zxmvd <zx_mailhot@hotmail.com>
2020-11-04 20:25:20 +1100

    update setting

--- wiki/settings.py
+++ wiki/settings.py
@@ -22,7 +22,7 @@ STATIC_URL = '/static/encyclopedia'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static/encyclopedia'),
+    os.path.join(BASE_DIR, 'static'),
 )
 
 # Quick-start development settings - unsuitable for production
